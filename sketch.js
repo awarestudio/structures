7dafa3572c913605f68aab483c4c67d60a9e1304
awarestudio <awarestudio.cc@gmail.com>
2023-04-22 15:50:07 +0200

    Inserted QuadStructure inside pattern2D library

--- sketch.js
+++ sketch.js
@@ -56,39 +56,3 @@ function drawNoisyQuad(layer, center, size, damping=1) {
 	q.render(layer)
 }
 
-class QuadStructure{
-	constructor(points) {
-		this.points = points // [topLeft, topRight, bottomRight, bottomLeft]
-	}
-
-	static regular(center, size) {
-		let relativePostions = [createVector(-size/2, -size/2), createVector(size/2, -size/2), createVector(size/2, size/2), createVector(-size/2, size/2)]
-		let points = []
-
-		for (let index = 0; index < relativePostions.length; index++) {
-			const point = p5.Vector.add(center, relativePostions[index])
-			points.push(point)
-		}
-		
-		return new QuadStructure(points)
-	}
-
-	static noisy(center, size, vertexNoiseRatio=0.5, translationNoiseRatio=1) {
-		let translationMagnitude = random(translationNoiseRatio) * size
-		let translatedCenter = center.add(p5.Vector.random2D().mult(translationMagnitude))
-
-		quad = QuadStructure.regular(translatedCenter, size)
-
-		for (let index = 0; index < 4; index++) {
-			let noiseMagnitude = random(vertexNoiseRatio) * size
-			quad.points[index].add(p5.Vector.random2D().mult(noiseMagnitude))
-		}
-
-		return quad
-	}
-
-	render(layer) {
-		layer.quad(this.points[0].x, this.points[0].y, this.points[1].x, this.points[1].y,
-			this.points[2].x, this.points[2].y, this.points[3].x, this.points[3].y)
-	}
-}
