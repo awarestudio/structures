1b8f2d0fd73bcbaef2c0cb35e7951348cf472398
awarestudio <awarestudio.cc@gmail.com>
2023-04-23 13:39:54 +0200

    Added pad and final pattern

--- sketch.js
+++ sketch.js
@@ -4,26 +4,25 @@ function setup() {
 	rectanglesLayer.angleMode(DEGREES)
 	rectanglesLayer.noFill()
 
-	COLORS = [
-		color(34, 116, 165),
-		color(221, 133, 44),
-		"black"
-	]
-	COLORS_W = [
-		33,
-		33,
-		33
-	]
-
 	noLoop()
+
+	N_SQUARES_MAX = linspace(0, 20, 20, false)
+	N_SQUARES_WEIGHTS = Array(20).fill(1)
+
+	N_SQUARES_WEIGHTS[0] = 1 // no
+	N_SQUARES_WEIGHTS.fill(2, 1, 6) // low
+	N_SQUARES_WEIGHTS.fill(1, 6, 11) // med
+	N_SQUARES_WEIGHTS.fill(2, 11, 16) // high
+	N_SQUARES_WEIGHTS.fill(1, 16, 20) // very high
 }
 
 function draw() {
 	createCanvas(CANVAS.WIDTH, CANVAS.HEIGHT)
 	background(BACKGROUND_COLOR)
+	rectanglesLayer.background(BACKGROUND_COLOR)
 
-	topLeft = createVector(CANVAS.MARGINS_POSITION.LEFT+SQUARE_SIZE/2, CANVAS.MARGINS_POSITION.TOP+SQUARE_SIZE/2)
-	positionsGrid = Grid2D.fromOnePoint(topLeft, N_SQUARES_V, N_SQUARES_H, SQUARE_SIZE, SQUARE_SIZE)
+	topLeft = createVector(CANVAS.MARGINS_POSITION.LEFT+SQUARE_STEP/2, CANVAS.MARGINS_POSITION.TOP+SQUARE_STEP/2)
+	positionsGrid = Grid2D.fromOnePoint(topLeft, N_SQUARES_V, N_SQUARES_H, SQUARE_STEP, SQUARE_STEP)
 
 	for (let vIndex = 0; vIndex < positionsGrid.points.length; vIndex++) {
 		const hpoints = positionsGrid.points[vIndex]
@@ -31,28 +30,13 @@ function draw() {
 		for (let hIndex = 0; hIndex < hpoints.length; hIndex++) {
 			const position = hpoints[hIndex]
 
-			span = map(hIndex, 0, hpoints.length, 0, PI)
-			// s = -5*sin(span+sin(span-PI)-PI)
-			s = 5*sin(span)+1
-			// s = 1
-
-			// -sin(x+sin(x-pi)-pi)
+			s = randomWeighted(N_SQUARES_MAX, N_SQUARES_WEIGHTS)
 			
 			for (let index = 0; index < s; index++) {
-				color = randomWeighted(COLORS, COLORS_W)
-				rectanglesLayer.stroke(color)
-				drawNoisyQuad(rectanglesLayer, position, SQUARE_SIZE, 0.5)
+				q = QuadStructure.noisy(position, SQUARE_SIZE, 0.125, 0.05)
+				q.render(rectanglesLayer)
 			}
 		}
 	}
-
 	image(rectanglesLayer, 0, 0)
-
-	// positionsGrid.debug()changechange
 }
-
-function drawNoisyQuad(layer, center, size, damping=1) {
-	q = QuadStructure.noisy(center, size, 0.1, 0.1)
-	q.render(layer)
-}
-
